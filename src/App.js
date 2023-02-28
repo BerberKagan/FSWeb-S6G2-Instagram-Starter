219884ca463c25aa0eb1e7e1d6567bf371562103
BerberKagan <kaganberbero@gmail.com>
2023-02-28 17:10:14 +0300

    Checked off my finished tasks

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@
 */
 
 // State hook u import edin
-import React from 'react';
-
+import React, { useState } from 'react';
+import AramaÇubuğu from "./bileşenler/AramaÇubuğu/AramaÇubuğu";
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
+import Gönderiler from './bileşenler/Gönderiler/Gönderiler';
 // sahteVeri'yi import edin
+import sahteVeri from './sahte-veri';
 import './App.css';
 
 const App = () => {
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
+  const [aramaKriteri, setAramaKriteri] = useState(null);
+
   const gonderiyiBegen = gonderiID => {
+    gonderiler.find((e) => e.id == gonderiID).likes += 1;
+    setGonderiler([...sahteVeri]);
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
       Bu fonksiyon, belirli bir gönderinin beğeni sayısını artırılmasına olanak sağlamak amacıyla iç içe geçmiş bileşenlere aktarılır.
 
-	  "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
+    "setGonderi" yi çağırın ve state ine "posts.map" çağrısını iletin.
       `map` içine iletilen callback aşağıdaki mantığı gerçekleştirir:
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
@@ -32,6 +38,8 @@ const App = () => {
 
   return (
     <div className='App'>
+      <AramaÇubuğu arama={aramaKriteri} />
+      <Gönderiler gonderiyiBegen={gonderiyiBegen} gonderiler={gonderiler} />
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
     </div>
